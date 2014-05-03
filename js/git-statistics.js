a042d765f426edd791c5282e4499f5a25f7549c7
Anders Windelhed <anders@windelhed.se>
2014-05-03 11:48:49 +0200

    Added js script for angular stuff and added angular attributes to html

---
+++ js/git-statistics.js
@@ -0,0 +1,12 @@
+var app = angular.module('gitStatisticsApp', []);
+
+function StatisticsCtrl($scope, $http) {
+    $scope.statistics = [];
+
+    var httpRequest = $http({
+        method: 'GET',
+        url: '/test-data.json'
+    }).success(function(data, status) {
+        $scope.statistics = data;
+    });
+}
