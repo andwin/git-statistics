3b0187fb2930a71f091f00dde0e91d521eb644c0
Anders Windelhed <anders@windelhed.se>
2014-05-06 17:28:03 +0200

    Refactoring the updater to use async.js

--- classes/updater.js
+++ classes/updater.js
@@ -1,13 +1,31 @@
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 var GitStatistics = require("../classes/gitStatistics.js");
 
 function Updater() {
   this.updateData = function() {
+    var data = {};
+    data.repos = [];
+    
     var repos = this.getAllRepos();
-    repos.forEach(function(repo) {
-      var gitStatistics = new GitStatistics(repo);
-      gitStatistics.updateRepo(function() {});
+    var gitStatisticsArray = new Array();
+    repos.forEach(function(repoPath) {
+      gitStatisticsArray.push(new GitStatistics(repoPath));
+    });
+
+    async.map(gitStatisticsArray, function(gitStatistics, callback) {
+      gitStatistics.updateRepo(function () {
+        var repoData = {};
+        repoData.name = path.basename(gitStatistics.repoPath);
+
+        gitStatistics.get10LatestCommits(function(latestCommits) {
+          repoData.latestCommits = latestCommits;
+          callback(null, repoData);
+        });
+      });
+    }, function(err, results) {
+      console.log(results);
     });
   }
 
@@ -17,10 +35,10 @@ function Updater() {
     files.forEach(function(file) {
       var repoPath = path.join('./repos/', file);
       if(fs.lstatSync(repoPath).isDirectory()) {
-        repos.push(repoPath);        
+        repos.push(repoPath);
       }
     });
-    
+
     return repos;
   }
 }
