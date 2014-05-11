6beb915be312670b4dce8f92bc36cf76fe34e69a
Anders Windelhed <anders@windelhed.se>
2014-05-11 21:31:54 +0200

    Added constructor that sets up tmp dir

--- tests/testRepoHelper.js
+++ tests/testRepoHelper.js
@@ -1,14 +1,11 @@
 var path = require('path');
+var ncp = require('ncp').ncp;
 
 function TestRepoHelper() {
   var testRepoDir;
   var self = this;
 
   this.setupTestRepo = function(repoName, callback) {
-    this.createTmpDir();
-
-    var ncp = require('ncp').ncp;
-
     // Copy test repo to tmp dir
     ncp(path.join(__dirname, 'repos', repoName), self.testRepoDir.path, function(err) {
       if(err) throw err;
@@ -18,10 +15,6 @@ function TestRepoHelper() {
   };
 
   this.setupAllTestRepos = function(callback) {
-    this.createTmpDir();    
-
-    var ncp = require('ncp').ncp;
-
     // Copy all test repos to tmp dir
     ncp(path.join(__dirname, 'repos'), self.testRepoDir.path, function(err) {
       if(err) throw err;
@@ -34,11 +27,15 @@ function TestRepoHelper() {
     var temporary = require('temporary');
     self.testRepoDir = new temporary.Dir();
     console.log(self.testRepoDir.path);
-  }
+  }  
 
   this.cleanup = function() {
     this.testRepoDir.rmdir();
   }
+
+  var __construct = function() {
+    self.createTmpDir();
+  }()
 }
 
 module.exports = TestRepoHelper;
