105b356d62eea505114bbcecafcce73d55f93e43
Anders Windelhed <anders@windelhed.se>
2014-05-24 15:48:16 +0200

    Rewrote gitStatistics to use nodes class pattern

--- classes/gitStatistics.js
+++ classes/gitStatistics.js
@@ -5,114 +5,111 @@ const
 
 function GitStatistics(repoPath) {
   this.repoPath = repoPath;
+};
 
-  this.getAll = function(callback) {
-  }
+GitStatistics.prototype.get10LatestCommits = function(callback) {
+  var limit = 10;
+  var command = 'git --git-dir=' + this.repoPath + ' log --pretty=format:\'{"commit": "%h","author": "%an <%ae>","date": "%ad","message": "%f"},\' -n' + limit;
+  var child = exec(command, function (err, stdout, stderr) {
+    var data = '[' + stdout.substring(0, stdout.length - 1) + ']';
+    callback(JSON.parse(data));
+  });
+};
 
-  this.get10LatestCommits = function(callback) {
-    var limit = 10;
-    var command = 'git --git-dir=' + this.repoPath + ' log --pretty=format:\'{"commit": "%h","author": "%an <%ae>","date": "%ad","message": "%f"},\' -n' + limit;
-    var child = exec(command, function (err, stdout, stderr) {
-      var data = '[' + stdout.substring(0, stdout.length - 1) + ']';
-      callback(JSON.parse(data));
+GitStatistics.prototype.getTop10Committers = function(callback) {
+  var limit = 10;
+  var command = 'git --git-dir=' + this.repoPath + ' shortlog -s < /dev/tty | sort -rn | head -n' + limit;
+  var child = exec(command, function (err, stdout, stderr) {
+    if(err) throw err;
+
+    var committers = [];
+
+    var lines = stdout.split("\n");
+    lines.forEach(function(line) {
+      var regex = /(\d+)\s(.+)/
+      var result = line.match(regex);
+      if(result) {
+        var committer = {};
+        committer.numberOfCommits = result[1];
+        committer.committer = result[2];
+        committers.push(committer);
+      }
     });
-  }
 
-  this.getTop10Committers = function(callback) {
-    var limit = 10;
-    var command = 'git --git-dir=' + this.repoPath + ' shortlog -s < /dev/tty | sort -rn | head -n' + limit;
-    var child = exec(command, function (err, stdout, stderr) {
-      if(err) throw err;
+    callback(committers);
+  });
+};
 
-      var committers = [];
+GitStatistics.prototype.getMostRecentTags = function(callback) {
+  let
+    limit = 5,
+    command = 'git --git-dir=' + this.repoPath + ' for-each-ref --format="%(refname:short) | %(committerdate) | %(taggerdate) | %(subject)" refs/tags --sort=-committerdate --count=' + limit;
 
-      var lines = stdout.split("\n");
-      lines.forEach(function(line) {
-        var regex = /(\d+)\s(.+)/
-        var result = line.match(regex);
-        if(result) {
-          var committer = {};
-          committer.numberOfCommits = result[1];
-          committer.committer = result[2];
-          committers.push(committer);
+  let child = exec(command, function (err, stdout, stderr) {
+    if(err) throw err;
+
+    let tags = [];
+    let lines = stdout.trim().split("\n");
+    lines.forEach(function(line) {
+      if(line.trim() == '') {
+        return;
+      }
+
+      var result = line.split("|");
+      if(result) {
+        let tag = {};
+        tag.name = result[0].trim();
+        tag.date = result[1].trim();
+        if(tag.date == '') {
+          tag.date = result[2].trim();
         }
-      });
-
-      callback(committers);
+        tag.message = result[3].trim();
+        tags.push(tag);
+      }
     });
-  }
 
-  this.getMostRecentTags = function(callback) {
-    let
-      limit = 5,
-      command = 'git --git-dir=' + this.repoPath + ' for-each-ref --format="%(refname:short) | %(committerdate) | %(taggerdate) | %(subject)" refs/tags --sort=-committerdate --count=' + limit;
+    callback(tags);
+  });
+}
 
-    let child = exec(command, function (err, stdout, stderr) {
-      if(err) throw err;
+GitStatistics.prototype.getMostRecentBranches = function(callback) {
+  let
+    limit = 5,
+    command = 'git --git-dir=' + this.repoPath + ' for-each-ref --sort=-committerdate refs/heads/ --format="%(refname:short) | %(committerdate) | %(authorname) | %(authoremail) | %(subject)" --count=' + limit;
 
-      let tags = [];
-      let lines = stdout.trim().split("\n");
-      lines.forEach(function(line) {
-        if(line.trim() == '') {
-          return;
-        }
+  let child = exec(command, function (err, stdout, stderr) {
+    if(err) throw err;
 
-        var result = line.split("|");
-        if(result) {
-          let tag = {};
-          tag.name = result[0].trim();
-          tag.date = result[1].trim();
-          if(tag.date == '') {
-            tag.date = result[2].trim();
-          }
-          tag.message = result[3].trim();
-          tags.push(tag);
-        }
-      });
+    let branches = [];
+    let lines = stdout.trim().split("\n");
+    lines.forEach(function(line) {
+      if(line.trim() == '') {
+        return;
+      }
 
-      callback(tags);
+      var result = line.split("|");
+      if(result) {
+        let branch = {};
+        branch.name = result[0].trim();
+        branch.date = result[1].trim();
+        branch.authorName = result[2].trim();
+        branch.authorEmail = result[3].trim();
+        branch.message = result[4].trim();
+        branches.push(branch);
+      }
     });
-  }
 
-  this.getMostRecentBranches = function(callback) {
-    let
-      limit = 5,
-      command = 'git --git-dir=' + this.repoPath + ' for-each-ref --sort=-committerdate refs/heads/ --format="%(refname:short) | %(committerdate) | %(authorname) | %(authoremail) | %(subject)" --count=' + limit;
+    callback(branches);
+  });
+}
 
-    let child = exec(command, function (err, stdout, stderr) {
-      if(err) throw err;
+GitStatistics.prototype.updateRepo = function(callback) {
+  var command = 'git --git-dir=' + this.repoPath + ' fetch --prune';
+  var child = exec(command, function (err, stdout, stderr) {
+    if(err) throw err;
 
-      let branches = [];
-      let lines = stdout.trim().split("\n");
-      lines.forEach(function(line) {
-        if(line.trim() == '') {
-          return;
-        }
-
-        var result = line.split("|");
-        if(result) {
-          let branch = {};
-          branch.name = result[0].trim();
-          branch.date = result[1].trim();
-          branch.authorName = result[2].trim();
-          branch.authorEmail = result[3].trim();
-          branch.message = result[4].trim();
-          branches.push(branch);
-        }
-      });
-
-      callback(branches);
-    });
-  }
-
-  this.updateRepo = function(callback) {
-    var command = 'git --git-dir=' + this.repoPath + ' fetch --prune';
-    var child = exec(command, function (err, stdout, stderr) {
-      if(err) throw err;
-
-      callback();
-    });
-  }
+    callback();
+  });
 }
 
 module.exports = GitStatistics;
