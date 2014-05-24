67885fb58d504f7f9076cc523007662f0ea0cec7
Anders Windelhed <anders@windelhed.se>
2014-05-24 15:48:16 +0200

    Rewrote updater to use nodes class pattern

--- classes/updater.js
+++ classes/updater.js
@@ -6,11 +6,11 @@ const
   GitStatistics = require('../classes/gitStatistics.js');
 
 function Updater(pathToReposDir) {
-  let self = this;
   this.pathToReposDir = pathToReposDir;
+}
 
-  this.statisticsSections = function(gitStatistics) {
-    return [
+Updater.prototype.statisticsSections = function(gitStatistics) {
+  return [
     function(callback) {
       gitStatistics.updateRepo(function() {
         callback(null, null);
@@ -36,101 +36,104 @@ function Updater(pathToReposDir) {
         callback(null, { mostRecentBranches: mostRecentBranches });
       });
     }
-  ]};
+  ]
+};
 
-  this.updateData = function(done) {
-    var repos = this.getAllRepos();
-    var gitStatisticsArray = new Array();
-    repos.forEach(function(repoName) {
-      gitStatisticsArray.push(new GitStatistics(path.join(self.pathToReposDir, repoName)));
-    });
+Updater.prototype.updateData = function(done) {
+  var self = this;
+  var repos = this.getAllRepos();
+  var gitStatisticsArray = new Array();
 
-    async.map(gitStatisticsArray, function(gitStatistics, repoCallback) {
-      async.series(
-        self.statisticsSections(gitStatistics),
-        function(err, results) {
-          var repoData = {};
-          repoData[path.basename(gitStatistics.repoPath)] = {};
-          self.formatResults(repoData[path.basename(gitStatistics.repoPath)], results);
+  repos.forEach(function(repoName) {
+    gitStatisticsArray.push(new GitStatistics(path.join(self.pathToReposDir, repoName)));
+  });
 
-          repoCallback(null, repoData);
-        }
-      );
-    }, function(err, results) {
-      var data = {};
-      data.repos = repos;
-      self.formatResults(data, results);
+  async.map(gitStatisticsArray, function(gitStatistics, repoCallback) {
+    async.series(
+      self.statisticsSections(gitStatistics),
+      function(err, results) {
+        var repoData = {};
+        repoData[path.basename(gitStatistics.repoPath)] = {};
+        self.formatResults(repoData[path.basename(gitStatistics.repoPath)], results);
 
-      self.calculateCombinedStatistics(data, function(combinedStatistics) {
-        self.formatResults(data, [combinedStatistics]);
-        data.repos.push('all');
-        done(data);
-      });
-    });
-  }
-
-  this.getAllRepos = function() {
-    var repos = new Array();
-    var files = fs.readdirSync(self.pathToReposDir);
-    files.forEach(function(file) {
-      var repoPath = path.join(self.pathToReposDir, file);
-      if(fs.lstatSync(repoPath).isDirectory()) {
-        repos.push(file);
+        repoCallback(null, repoData);
       }
+    );
+  }, function(err, results) {
+    var data = {};
+    data.repos = repos;
+    self.formatResults(data, results);
+
+    self.calculateCombinedStatistics(data, function(combinedStatistics) {
+      self.formatResults(data, [combinedStatistics]);
+      data.repos.push('all');
+      done(data);
     });
+  });
+}
 
-    return repos;
-  }
+Updater.prototype.getAllRepos = function() {
+  var self = this;
+  var repos = new Array();
+  var files = fs.readdirSync(this.pathToReposDir);
+  files.forEach(function(file) {
+    var repoPath = path.join(self.pathToReposDir, file);
+    if(fs.lstatSync(repoPath).isDirectory()) {
+      repos.push(file);
+    }
+  });
 
-  this.formatResults = function(data, results) {
-    for (var i in results) {
-      for (var c in results[i]) {
-        data[c] = results[i][c];
-      }
+  return repos;
+}
+
+Updater.prototype.formatResults = function(data, results) {
+  for (var i in results) {
+    for (var c in results[i]) {
+      data[c] = results[i][c];
     }
   }
+}
 
-  this.calculateCombinedStatistics = function(data, callback) {
-    var combinedStatistics = {};
+Updater.prototype.calculateCombinedStatistics = function(data, callback) {
+  var combinedStatistics = {};
 
-    // latestCommits
-    var allCommits = self.getCombinedStatisticsSection(data, 'latestCommits');
-    allCommits.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
-    combinedStatistics.latestCommits = allCommits.slice(0, 10);
+  // latestCommits
+  var allCommits = this.getCombinedStatisticsSection(data, 'latestCommits');
+  allCommits.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
+  combinedStatistics.latestCommits = allCommits.slice(0, 10);
 
-    // top10Committers
-    var allCommitters = self.getCombinedStatisticsSection(data, 'top10Committers');
-    allCommitters.sort(function(a,b) { return parseInt(b.numberOfCommits) - parseInt(a.numberOfCommits) } );
-    combinedStatistics.top10Committers = allCommitters.slice(0, 10);
+  // top10Committers
+  var allCommitters = this.getCombinedStatisticsSection(data, 'top10Committers');
+  allCommitters.sort(function(a,b) { return parseInt(b.numberOfCommits) - parseInt(a.numberOfCommits) } );
+  combinedStatistics.top10Committers = allCommitters.slice(0, 10);
 
-    // mostRecentTags
-    var allTags = self.getCombinedStatisticsSection(data, 'mostRecentTags');
-    allTags.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
-    combinedStatistics.mostRecentTags = allTags.slice(0, 5);
+  // mostRecentTags
+  var allTags = this.getCombinedStatisticsSection(data, 'mostRecentTags');
+  allTags.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
+  combinedStatistics.mostRecentTags = allTags.slice(0, 5);
 
-    // mostRecentBranches
-    var allBranches = self.getCombinedStatisticsSection(data, 'mostRecentBranches');
-    allBranches.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
-    combinedStatistics.mostRecentBranches = allBranches.slice(0, 5);
+  // mostRecentBranches
+  var allBranches = this.getCombinedStatisticsSection(data, 'mostRecentBranches');
+  allBranches.sort(function(a,b) { return new Date(a.date) - new Date(b.data) } );
+  combinedStatistics.mostRecentBranches = allBranches.slice(0, 5);
 
-    callback({all: combinedStatistics});
-  }
+  callback({all: combinedStatistics});
+}
 
-  this.getCombinedStatisticsSection = function(data, section) {
-    var items = [];
-    for(var i in data) {
-      if(data[i] == 'repos') {
-        continue;
-      }
-
-      for(var c in data[i][section]) {
-        var item = JSON.parse(JSON.stringify(data[i][section][c]));
-        item.repo = i;
-        items.push(item);
-      }
+Updater.prototype.getCombinedStatisticsSection = function(data, section) {
+  var items = [];
+  for(var i in data) {
+    if(data[i] == 'repos') {
+      continue;
     }
-    return items;
+
+    for(var c in data[i][section]) {
+      var item = JSON.parse(JSON.stringify(data[i][section][c]));
+      item.repo = i;
+      items.push(item);
+    }
   }
+  return items;
 }
 
 module.exports = Updater;
